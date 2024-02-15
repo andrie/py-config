10c5b49c56351ec22bd073f5313a2aca493b7c05
andrie <apdevries@gmail.com>
2024-02-15 17:53:22 +0000

    add: more docs and tests

--- docs/source/stubs/quickstart-stub.md
+++ docs/source/stubs/quickstart-stub.md
@@ -1,6 +1,17 @@
-Install `config`
 
-```{prompt} bash
+Installation
+
+The package is not yet available on PyPI, but you can install it from GitHub:
+
+```sh
 pip install git+https://github.com/andrie/py-config
 ```
+
+Tip: add this to your `requirements.txt` file:
+
+```sh
+config @ git+https://github.com/andrie/py-config
+```
+
+And then use `pip install -r requirements.txt` to install the package.
 
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,11 +1,29 @@
 import os
 import pytest
 from config import *
+from config.core import read_yaml
 
 
-def test_core():
+def test_get_env():
     assert get_env('TEST') == 'default'
     os.environ['TEST'] = 'test'
     assert get_env('TEST') == 'test'
 
     # assert "fail" == "pass"
+
+def test_config_get():
+    assert config_get('trials') == 5
+    assert config_get('trials', 'production') == 30
+
+
+def test_read_yaml():
+    #| hide
+    x = read_yaml(
+        """
+        trials: 1
+        expr: !expr os.getcwd()
+        # expr2: !expr invalid()
+        """
+    )
+    assert x['trials'] == 1
+    assert x['expr'] == os.getcwd()
